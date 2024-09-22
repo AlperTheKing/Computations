6f6374a5de7f26035c044ea1967a00ea075fe9aa
AlperTheKing <alper@mercurycodelab.com>
2024-09-22 12:51:19 +0300

    .

--- CardanoTriplets.cpp
+++ CardanoTriplets.cpp
@@ -1,62 +1,58 @@
 #include <iostream>
-#include <set>
+#include <unordered_set>
 #include <tuple>
-#include <chrono>
 #include <cmath>
+#include <chrono>
 #include <thread>
+#include <vector>
 #include <mutex>
 #include <atomic>
 
 typedef long long ll;
 std::mutex triplet_mutex;  // Mutex to protect shared triplet data
 
-// Function to generate square divisors of a number
-std::vector<ll> get_square_divisors(ll n) {
-    std::vector<ll> divisors;
-    for (ll i = 1; i * i <= n; ++i) {
-        if (n % (i * i) == 0) {
-            divisors.push_back(i);
-        }
+// Custom hash function for tuple of (a, b, c)
+struct hash_triplet {
+    std::size_t operator()(const std::tuple<ll, ll, ll>& triplet) const {
+        auto h1 = std::hash<ll>()(std::get<0>(triplet));
+        auto h2 = std::hash<ll>()(std::get<1>(triplet));
+        auto h3 = std::hash<ll>()(std::get<2>(triplet));
+        return h1 ^ (h2 << 1) ^ (h3 << 2);
     }
-    return divisors;
-}
+};
 
-// Container to store unique triplets (using a set to eliminate duplicates)
-std::set<std::tuple<ll, ll, ll>> triplets;
-
-// Function to be executed by each thread
-void find_cardano_triplets(ll start_a, ll limit, std::atomic<bool>& done) {
+// Function to find valid Cardano Triplets (a, b, c) for a range of `a` values
+void find_cardano_triplets(ll start_a, ll limit, std::atomic<bool>& done, std::unordered_set<std::tuple<ll, ll, ll>, hash_triplet>& local_triplets, ll step) {
     ll a = start_a;
     while (!done.load()) {
-        // Calculate N = 8a^3 + 15a^2 + 6a - 1
-        ll a_squared = a * a;
-        ll a_cubed = a_squared * a;
-        ll N = 8 * a_cubed + 15 * a_squared + 6 * a - 1;
+        ll u = (2 * a - 1) / 3;
+        if (u % 2 == 0) {
+            a += step;
+            continue;  // u must be odd
+        }
 
-        // Check if N is divisible by 27
-        if (N % 27 == 0) {
-            // Calculate N / 27
-            ll N27 = N / 27;
+        ll lhs = u * u * u + a * a;  // u^3 + a^2
+        bool found_triplet_for_a = false;  // Track whether at least one triplet was found for this value of a
 
-            // Get all square divisors of N / 27
-            std::vector<ll> divisors = get_square_divisors(N27);
-
-            // For each divisor b, calculate the corresponding c value
-            for (ll b : divisors) {
-                ll b_squared = b * b;
-                ll c = N27 / b_squared;
-                ll sum = a + b + c;
-                if (sum <= limit) {
+        for (ll b = 1; b * b <= lhs; ++b) {
+            if (lhs % (b * b) == 0) {
+                ll c = lhs / (b * b);
+                if (a + b + c <= limit) {
                     // Lock mutex to safely add the unique triplet to the shared container
                     std::lock_guard<std::mutex> guard(triplet_mutex);
-                    triplets.insert(std::make_tuple(a, b, c));  // Add the triplet to the set
-                } else if (a > limit) {
-                    done.store(true);  // Signal other threads to stop
-                    break;
+                    local_triplets.insert(std::make_tuple(a, b, c));  // Add the triplet to the set
+                    found_triplet_for_a = true;
                 }
             }
         }
-        a += 3 * std::thread::hardware_concurrency();  // Increase a by the total step size
+
+        // If no valid triplet was found for the current value of a and the sum exceeds the limit, we stop
+        if (!found_triplet_for_a && a > limit) {
+            done.store(true);  // Signal threads to stop
+            break;
+        }
+
+        a += step;  // Skip over other threads' values
     }
 }
 
@@ -64,21 +60,23 @@ int main() {
     // Start measuring time
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    ll limit = 110000000;
-
-    // Use atomic boolean to signal threads to stop
+    ll limit = 110000000;  // The sum a + b + c must be less than or equal to this limit
     std::atomic<bool> done(false);
 
     // Get the number of hardware threads available
     unsigned int num_threads = std::thread::hardware_concurrency();
     if (num_threads == 0) num_threads = 4;  // Default to 4 if the system cannot determine it
 
+    // Each thread will have its own local set of triplets
+    std::vector<std::unordered_set<std::tuple<ll, ll, ll>, hash_triplet>> thread_triplets(num_threads);
+
     std::vector<std::thread> threads;
 
     // Launch multiple threads with different starting points for `a`
+    ll step = num_threads * 3;
     for (unsigned int i = 0; i < num_threads; ++i) {
         ll thread_start_a = 2 + 3 * i;  // Each thread starts with a different value of `a`
-        threads.push_back(std::thread(find_cardano_triplets, thread_start_a, limit, std::ref(done)));
+        threads.emplace_back(find_cardano_triplets, thread_start_a, limit, std::ref(done), std::ref(thread_triplets[i]), step);
     }
 
     // Join all threads to ensure they complete execution
@@ -86,6 +84,12 @@ int main() {
         if (th.joinable()) {
             th.join();
         }
+    }
+
+    // Combine results from all threads
+    std::unordered_set<std::tuple<ll, ll, ll>, hash_triplet> triplets;
+    for (const auto& local_triplets : thread_triplets) {
+        triplets.insert(local_triplets.begin(), local_triplets.end());
     }
 
     // Output the total number of unique triplets found
